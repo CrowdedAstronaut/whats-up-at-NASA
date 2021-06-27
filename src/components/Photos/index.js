dc88ee4c65a50af7d1efd74d727dc817632cecdf
CrowdedAstronaut <ross@johnrossphillips.com>
2021-06-27 10:23:48 -0500

    Imported hardcoded data using fetch request

--- src/components/Photos/index.js
+++ src/components/Photos/index.js
@@ -1,7 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Photos() {
   const [photos, setPhotos] = useState([]);
+  const getPhotos = async () => {
+    const apiEndPoint = `https://audubon-api.herokuapp.com/api/birds`;
+    try {
+      const response = await fetch(apiEndPoint); //<-- the url as a string
+      const data = await response.json(); // Wait for the response and convert it to json
+      setPhotos(data);
+    } catch (err) {
+      // Catch and log any errors to the console
+      console.error(err);
+    }
+  };
+
+  // call that function in your useEffect
+  useEffect(() => {
+    getPhotos();
+  }, []);
   return (
     <div>
       <ul>
