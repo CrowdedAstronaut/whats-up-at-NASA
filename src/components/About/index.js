cc09373f313a3172864737c1c0f78aabfc61be6f
CrowdedAstronaut <ross@johnrossphillips.com>
2021-06-29 09:58:23 -0500

    Added NavLink to the About page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,12 +1,5 @@
+import { NavLink } from "react-router-dom";
+
 export default function About() {
-  return (
-    <div>
-      <h3>Welcome to What's Up at NASA</h3>
-      <p>
-        This application queries the NASA Photo of the Day API and returns 25
-        results to the user. If the user clicks on a photo, the app will return
-        details about that photo from the NASA API.
-      </p>
-    </div>
-  );
+  return <NavLink to="/about">About</NavLink>;
 }
